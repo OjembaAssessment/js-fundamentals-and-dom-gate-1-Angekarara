0c961b1538b506814ce0313b6d0f7b9aa6c7d128
Angekarara <mbabange2020@gmail.com>
2023-07-24 09:53:10 +0200

    index

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,6 +1,9 @@
 const substraction = document.querySelector("#substract")
 const myTime = document.querySelector("#myTime")
-const addition = document.querySelector("#myTime")
+const addition = document.querySelector("#addition")
+const reset = document.querySelector("#reset")
+const start = document.querySelector("start")
+
 
 
 
