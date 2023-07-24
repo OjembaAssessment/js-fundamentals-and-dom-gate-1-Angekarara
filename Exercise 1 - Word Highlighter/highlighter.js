73cd78d4b0e621392734d8c820c014fa1afb3f03
Angekarara <mbabange2020@gmail.com>
2023-07-24 13:06:41 +0200

    commit

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,19 @@
-console.log("Exercise 1 - Word Highlighter");
+const paragraph = document.getElementById("myParagraph");
+const words = paragraph.textContent.toLowerCase().match(/\b\w+\b/g);
+const wordFrequency = words.reduce((freq, word) => (freq[word] = -~freq[word], freq), {});
+
+Object.keys(wordFrequency)
+  .sort((a, b) => wordFrequency[b] - wordFrequency[a])
+  .slice(0, 5)
+  .forEach(word => {
+    const span = document.createElement("span");
+    span.textContent = word;
+    span.style.backgroundColor = "yellow";
+    if (wordFrequency[word] > 1) {
+      span.style.textDecoration = "underline";
+    }
+    paragraph.innerHTML = paragraph.innerHTML.replace(
+      new RegExp(`${word}`, "gi"),
+      span.outerHTML
+    );
+  });
